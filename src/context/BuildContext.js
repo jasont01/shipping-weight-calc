be36ed8d48a0ff8942708d5b41dd57c3276c22b1
Jason Thompson <jason@jasont.us>
2023-01-14 10:24:14 -0500

    add BuildContext / WIP

---
+++ src/context/BuildContext.js
@@ -0,0 +1,55 @@
+import { createContext, useReducer } from 'react'
+
+import data from '../data.json'
+
+const DEFAULT_STATE = {
+  build: {
+    panelType: data.cabinets.panels[0],
+    panels:
+      data.cabinets.size[0].interiorPanels + data.cabinets.size[0].doorPanels,
+    size: data.cabinets.size[0],
+    config: data.cabinets.config[0],
+    mount: data.cabinets.mount[0],
+    qty: 1,
+  },
+  accessories: data.accessories
+    .map((category) => category.items.map((item) => ({ ...item, qty: 0 })))
+    .flat(),
+  shipping: [
+    ...data.shipping.pallets.map((size) => ({ ...size, qty: 0 })),
+    ...data.shipping.misc.map((item) => ({ ...item, qty: 0 })),
+  ],
+}
+
+export const BuildContext = createContext()
+
+const buildReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_BUILD':
+      return {}
+
+    case 'SET_ACCESSORIES':
+      return {}
+
+    case 'SET_SHIPPING':
+      return {}
+
+    case 'RESET':
+      return DEFAULT_STATE
+
+    default:
+      return state
+  }
+}
+
+const BuildContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(buildReducer, DEFAULT_STATE)
+
+  return (
+    <BuildContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </BuildContext.Provider>
+  )
+}
+
+export default BuildContextProvider
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import BuildContextProvider from './context/BuildContext'
 import ShipmentContextProvider from './context/ShipmentContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <ShipmentContextProvider>
-      <App />
-    </ShipmentContextProvider>
+    <BuildContextProvider>
+      <ShipmentContextProvider>
+        <App />
+      </ShipmentContextProvider>
+    </BuildContextProvider>
   </React.StrictMode>
 )
